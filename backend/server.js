1229c9b587cddcf46aa3645e9602a14ef94ce155
yash <architect4646@gmail.com>
2026-04-22 10:38:49 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(cors({
-    origin:[ 'http://localhost:5173' ,    'https://foodie-express-eta.vercel.app' ],
+    origin:[ 'http://localhost:5173' ,    'https://food-delivery-frontend-enw5.onrender.com' ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
